0d5fc637abd93b60e9a2786e9a20072c8d4b3c7c
xueming998 <xueming998@users.noreply.github.com>
2020-11-19 15:00:03 +0800

    Update nav.inc.php

--- ICT1004-TartsNCakes/phpFiles/nav.inc.php
+++ ICT1004-TartsNCakes/phpFiles/nav.inc.php
@@ -26,9 +26,67 @@ Primary Buttons:
             <li class="nav-item">
                 <a class="nav-link" href="#dogs">About Us</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#cats">Account</a>
-            </li>
+           
+            
+            
+             <?php       
+               session_start();
+
+             if(!isset($_SESSION['whoami']))
+              {
+                   echo ' <li class="nav-item">
+                  <a class="nav-link" title="Login" href="Login.php">Login              
+                  </a>                  
+                  </li>';
+              }
+              else
+              {
+                   echo ' <li class="nav-item">
+                  <a class="nav-link" title="Logout" href="logout.php">Logout           
+                  </a>                  
+                  </li>';
+              }
+              
+           //open connection and select database
+           $config = parse_ini_file('../../private/db1-config.ini');
+           $conn = new mysqli($config['servername'], $config['username'],
+           $config['password'], $config['dbname']);
+  
+ 
+            $useremail = $_SESSION['whoami'];
+
+            $sql="SELECT * FROM cake_member where email ='$useremail'";
+
+            
+
+              ?>
+              
+               <?php       
+               session_start();
+
+             if(isset($_SESSION['whoami']))
+              {
+                   echo ' <li class="nav-item">
+                <a class="nav-link" title="view profile" href="profile.php">Account              
+                  </a>                  
+                  </li>';
+              }
+             
+           //open connection and select database
+           $config = parse_ini_file('../../private/db1-config.ini');
+           $conn = new mysqli($config['servername'], $config['username'],
+           $config['password'], $config['dbname']);
+  
+ 
+            $useremail = $_SESSION['whoami'];
+
+            $sql="SELECT * FROM cake_member where email ='$useremail'";
+
+            
+
+              ?>
+            
+            
                 <li class="nav-item">
                 <a class="nav-link" href="#cats">Shopping Cart</a>
             </li>
