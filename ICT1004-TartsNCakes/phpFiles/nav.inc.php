756afb8dbb6df6c215fe7f7c13c6f8daed0eeed5
xueming998 <xueming998@users.noreply.github.com>
2020-11-20 02:34:00 +0800

    nav with dropdown for account
    dropdown for account

--- ICT1004-TartsNCakes/phpFiles/nav.inc.php
+++ ICT1004-TartsNCakes/phpFiles/nav.inc.php
@@ -8,6 +8,38 @@ Primary Buttons:
     - Account (Drop Down)
 -->
 
+
+<style>
+
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: cornflowerblue;
+  min-width: 70px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: whitesmoke;
+  padding: 10px 25px;
+  text-decoration: none;
+  display: block;
+  font-size: 10px;
+}
+
+.dropdown-content a:hover {color: orange;}
+
+.dropdown:hover .dropdown-content {display: block;}
+
+</style>
+
 <nav id ="navbar"
      class="navbar sticky-top navbar-expand-sm navbar-light"
      style="background-color: #e3f2fd;">
@@ -42,11 +74,20 @@ Primary Buttons:
               else
               {
                    echo ' <li class="nav-item">
-                  <a class="nav-link" title="Logout" href="logout.php">Logout           
-                  </a>                  
+                       
+                   <div class="dropdown">
+                        <a class="nav-link" title="view profile" >Account</a>                   
+                        <div class="dropdown-content">
+                           <a href="profile.php">Profile</a>
+                           <a href="logout.php">Logout</a>                   
+                        </div>
+                   </div>
+                   
+
+                                 
                   </li>';
               }
-              
+             
            //open connection and select database
            $config = parse_ini_file('../../private/db1-config.ini');
            $conn = new mysqli($config['servername'], $config['username'],
@@ -66,10 +107,7 @@ Primary Buttons:
 
              if(isset($_SESSION['whoami']))
               {
-                   echo ' <li class="nav-item">
-                <a class="nav-link" title="view profile" href="profile.php">Account              
-                  </a>                  
-                  </li>';
+              
                    
                    echo ' <li class="nav-item">
                 <a class="nav-link" title="cart" href="Cart.php">Shopping Cart              
