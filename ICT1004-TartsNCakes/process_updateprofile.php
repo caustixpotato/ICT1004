ca1ead95db133b078de459b63fd6810e88ace1b0
xueming998 <xueming998@users.noreply.github.com>
2020-11-26 20:43:47 +0800

    updateprofile_email validation
    updateprofile_email validation no same email in database allow

--- ICT1004-TartsNCakes/process_updateprofile.php
+++ ICT1004-TartsNCakes/process_updateprofile.php
@@ -16,7 +16,24 @@ $newvalueaddress = $_POST['newaddress'];
 
 $newfname ="UPDATE cake_member SET fname = '$newvaluefname', lname = '$newvaluelname', email = '$newvalueemail', phoneno = '$newvaluephoneno', address = '$newvalueaddress'  WHERE userID = '$userid'";
 
+ $sql = "select * from cake_member where email = '$newvalueemail' ";
+ $search_result = mysqli_query($conn, $sql);
+ $emailfound = mysqli_num_rows($search_result);
+if($emailfound >= 1)
+   {
+     //fail
+   echo "<script>
+   alert('Email already been use try other email');
+   window.location.href='index.php';
+   </script>";
+    $success = false;
+     $newfname->close();
+   }
+   
+
 $Updateuser = mysqli_query($conn,$newfname);
+
+
 
 if($Updateuser >0)
 {
