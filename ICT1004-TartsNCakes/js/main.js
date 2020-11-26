70ab4d8b065862bd50bb0a4d8be654401749d736
Jwsh L <68888964+caustixpotato@users.noreply.github.com>
2020-11-26 15:54:04 +0800

    navbarfix

--- ICT1004-TartsNCakes/js/main.js
+++ ICT1004-TartsNCakes/js/main.js
@@ -11,36 +11,17 @@ const ID_POPUP = "id_popup";
 //const ID_MODAL = "id_modal";
 $(document).ready(function ()
 {
+    $(window).scroll(function () {
+        navbarchange();
+    });
     //Register event handlers for the thumbnail popup images.
-    document.getElementById("myBtn1").addEventListener("click", Btn1);
-    document.getElementById("myBtn2").addEventListener("click", Btn2);
-    document.getElementById("myBtn3").addEventListener("click", Btn3);
     console.log("JS loaded & Ready!");
 });
-
-//Singapore
-function Btn1() {
-  document.getElementById("card-img").src =
-  "http://media.marketwire.com/attachments/201404/243661_HelloWorldLogo.jpg";
-  document.getElementById("card-title").innerHTML = "Changed";
-  document.getElementById("card-body").innerHTML = "Hello JavaScript!";
-  document.getElementById("phone-link").href = "https://duckduckgo.com/";
-}
-
-//Malaysia
-function Btn2() {
-  document.getElementById("card-img").src =
-  "http://media.marketwire.com/attachments/201404/243661_HelloWorldLogo.jpg";
-  document.getElementById("card-title").innerHTML = "Changed2";
-  document.getElementById("card-body").innerHTML = "Hello JavaScript!";
-  document.getElementById("phone-link").href = "https://duckduckgo.com/";
-}
-
-//Taiwan
-function Btn3() {
-  document.getElementById("card-img").src =
-  "http://media.marketwire.com/attachments/201404/243661_HelloWorldLogo.jpg";
-  document.getElementById("card-title").innerHTML = "Changed3";
-  document.getElementById("card-body").innerHTML = "Hello JavaScript!";
-  document.getElementById("phone-link").href = "https://duckduckgo.com/";
+function navbarchange()
+{
+    if ($(window).scrollTop() >= 60) {
+        $('.navbar').css('background', 'white');
+    } else {
+        $('.navbar').css('background', 'transparent');
+    }
 }
