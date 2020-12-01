e584e2de854342f204350de0cfcaf5ae64d93076
ackillben1 <74306876+ackillben1@users.noreply.github.com>
2020-12-01 16:31:17 +0800

    Moved the Style and Script into respective js and css files

--- ICT1004-TartsNCakes/Store.php
+++ ICT1004-TartsNCakes/Store.php
@@ -7,6 +7,7 @@
               integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
               crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css" />
+        <link rel ="stylesheet" href="css/items.css"/>
 
 
 
@@ -23,96 +24,8 @@
         </script>
 
         <!-- Internal JS -->
-<!--        <script defer src="js/item.js"></script>-->
-        <style>
-            body {font-family: Arial, Helvetica, sans-serif;}
-
-            #myImg {
-                border-radius: 5px;
-                cursor: pointer;
-                transition: 0.3s;
-            }
-
-            #myImg:hover {opacity: 0.7;}
-
-            /* The Modal (background) */
-            .modal {
-                display: none; /* Hidden by default */
-                position: fixed; /* Stay in place */
-                z-index: 1; /* Sit on top */
-                padding-top: 100px; /* Location of the box */
-                left: 0;
-                top: 0;
-                width: 100%; /* Full width */
-                height: 100%; /* Full height */
-                overflow: auto; /* Enable scroll if needed */
-                background-color: rgb(0,0,0); /* Fallback color */
-                background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
-            }
-
-            /* Modal Content (image) */
-            .modal-content {
-                margin: auto;
-                display: block;
-                width: 80%;
-                max-width: 700px;
-            }
-
-            /* Caption of Modal Image */
-            #caption {
-                margin: auto;
-                display: block;
-                width: 80%;
-                max-width: 700px;
-                text-align: center;
-                color: #ccc;
-                padding: 10px 0;
-                height: 150px;
-            }
-
-            /* Add Animation */
-            .modal-content, #caption {  
-                -webkit-animation-name: zoom;
-                -webkit-animation-duration: 0.6s;
-                animation-name: zoom;
-                animation-duration: 0.6s;
-            }
-
-            @-webkit-keyframes zoom {
-                from {-webkit-transform:scale(0)} 
-                to {-webkit-transform:scale(1)}
-            }
-
-            @keyframes zoom {
-                from {transform:scale(0)} 
-                to {transform:scale(1)}
-            }
-
-            /* The Close Button */
-            .close {
-                position: absolute;
-                top: 15px;
-                right: 35px;
-                color: #f1f1f1;
-                font-size: 40px;
-                font-weight: bold;
-                transition: 0.3s;
-            }
-
-            .close:hover,
-            .close:focus {
-                color: #bbb;
-                text-decoration: none;
-                cursor: pointer;
-            }
-
-            /* 100% Image Width on Smaller Screens */
-            @media only screen and (max-width: 700px){
-                .modal-content {
-                    width: 100%;
-                }
-            }
-        </style>
+        <script src="js/item.js"></script>
+        
     </head>
     <body>
         <?php
@@ -260,36 +173,6 @@
         <?php
         include "footer.inc.php"; //add/include the content from footer.inc.php
         ?>
-        <script>
-            var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-            var images = document.getElementsByClassName("img-thumbnail");
-            var modalImg = document.getElementById("img01");
-            var captionText = document.getElementById("caption");
-            for (var i = 0; i < images.length; i++) {
-                var img = images[i];
-                img.onclick = function () {
-                    modal.style.display = "block";
-                    modalImg.src = this.src;
-                    captionText.innerHTML = this.alt;
-                }
-            }
-//
-//// Get the <span> element that closes the modal
-//            var span = document.getElementsByClassName("close")[0];
-//
-//// When the user clicks on <span> (x), close the modal
-//            span.onclick = function () {
-//                modal.style.display = "none";
-//            }
-
-            window.onclick = function (event) {
-                if (event.target === modal) {
-                    modal.style.display = "none";
-                }
-            }
-        </script>
     </body>
 </html>
 
--- ICT1004-TartsNCakes/js/item.js
+++ ICT1004-TartsNCakes/js/item.js
@@ -4,51 +4,80 @@
  * and open the template in the editor.
  */
 
-const ID_POPUP = "id_popup";
+//const ID_POPUP = "id_popup";
+//
+//$( document ).ready(function() 
+//{
+//
+//    registerEventHandler();
+//});
+//
+//function registerEventHandler()
+//{
+//	var thumbnails = document.getElementsByClassName("img-thumbnail"); //returns an array called thumbnails
+//        if (thumbnails !== null)
+//	{
+//		var counter = 0;
+//		for (var counter; counter < thumbnails.length; counter += 1)
+//		{
+//			var thumbnail = thumbnails[counter];
+//			thumbnail.addEventListener("click", togglePopup);
+//		}
+//	}
+//	else
+//	{
+//		console.log("No thumbnail images found!");
+//	}
+//}
+//
+//function togglePopup(event)
+//{
+//	var popup = document.getElementById(ID_POPUP);
+//	if (popup === null)
+//	{
+//		popup = document.createElement("span");
+//		popup.id = ID_POPUP;
+//		popup.setAttribute("class", "img-popup");
+//		var thumbnail = event.target;
+//		var image = thumbnail.src; 
+//		//var big_image = small_image.replace("_small", "_large");
+//		popup.innerHTML = '<div class = "container">'+
+//                                  '<img src=' + image + '>' +
+//                                  '<h3>'+ document.getElementsByClassName("desc").textContent +'</h3></a>'+
+//                                  '<a class="btn btn-primary" href="#" role="button">Add To Cart</div>';
+//		thumbnail.insertAdjacentElement("afterend", popup);
+//	}
+//	else
+//	{
+//		$("#" + ID_POPUP).remove();
+//	}
+//}
 
-$( document ).ready(function() 
-{
+var modal = document.getElementById("myModal");
 
-    registerEventHandler();
-});
-
-function registerEventHandler()
-{
-	var thumbnails = document.getElementsByClassName("img-thumbnail"); //returns an array called thumbnails
-        if (thumbnails !== null)
-	{
-		var counter = 0;
-		for (var counter; counter < thumbnails.length; counter += 1)
-		{
-			var thumbnail = thumbnails[counter];
-			thumbnail.addEventListener("click", togglePopup);
-		}
-	}
-	else
-	{
-		console.log("No thumbnail images found!");
-	}
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.getElementsByClassName("img-thumbnail");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
 }
+//
+//// Get the <span> element that closes the modal
+//            var span = document.getElementsByClassName("close")[0];
+//
+//// When the user clicks on <span> (x), close the modal
+//            span.onclick = function () {
+//                modal.style.display = "none";
+//            }
 
-function togglePopup(event)
-{
-	var popup = document.getElementById(ID_POPUP);
-	if (popup === null)
-	{
-		popup = document.createElement("span");
-		popup.id = ID_POPUP;
-		popup.setAttribute("class", "img-popup");
-		var thumbnail = event.target;
-		var image = thumbnail.src; 
-		//var big_image = small_image.replace("_small", "_large");
-		popup.innerHTML = '<div class = "container">'+
-                                  '<img src=' + image + '>' +
-                                  '<h3>'+ document.getElementsByClassName("desc").textContent +'</h3></a>'+
-                                  '<a class="btn btn-primary" href="#" role="button">Add To Cart</div>';
-		thumbnail.insertAdjacentElement("afterend", popup);
-	}
-	else
-	{
-		$("#" + ID_POPUP).remove();
-	}
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 }
