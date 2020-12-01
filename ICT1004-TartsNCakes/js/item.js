27d5fe370a249ef7dcd0b0fdddd2eafa7c4b42da
ackillben1 <74306876+ackillben1@users.noreply.github.com>
2020-12-01 20:15:10 +0800

    Added listeners for price, desc and name of product.

--- ICT1004-TartsNCakes/js/item.js
+++ ICT1004-TartsNCakes/js/item.js
@@ -58,23 +58,30 @@ var modal = document.getElementById("myModal");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var images = document.getElementsByClassName("img-thumbnail");
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var nameText = document.getElementById("name-content");
+var descModal = document.getElementById("desc-content");
+var priceModal = document.getElementById("price-content");
 for (var i = 0; i < images.length; i++) {
     var img = images[i];
     img.onclick = function () {
+        var j = this.id;
+        var desc = document.getElementById("idDesc" + j + "").textContent;
+        var price = document.getElementById("idPrice" + j + "").textContent;
         modal.style.display = "block";
         modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
+        nameText.innerHTML = this.alt;
+        descModal.innerHTML = desc;
+        priceModal.innerHTML = "$" + price + "";
     }
 }
-//
-//// Get the <span> element that closes the modal
-//            var span = document.getElementsByClassName("close")[0];
-//
-//// When the user clicks on <span> (x), close the modal
-//            span.onclick = function () {
-//                modal.style.display = "none";
-//            }
+
+// Get the <span> element that closes the modal
+//var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+//span.onclick = function () {
+//    modal.style.display = "none";
+//}
 
 window.onclick = function (event) {
     if (event.target === modal) {
